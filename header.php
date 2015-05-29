b7fbb8b2cc8f478ea74d7367d76fd51f7c51b8b4
Javier Caicedo <jicaicedocpe@gmail.com>
2015-05-29 12:41:10 -0500

    COD: Nueva interfaz menos RECARGADA

---
+++ header.php
@@ -0,0 +1,24 @@
+<header>
+  <div class="encabezado__image">
+    <figure class="encabezado__image__foto">
+      <img class="encabezado__image__foto--img" src="images/desayunos.jpg" alt="">
+    </figure>
+  </div>
+  <div class="panel_menu">
+    <nav class="menu">
+      <ul>
+        <li>  <img src="images/iconos/0190-menu.png" alt="">    
+          <ul>
+            <li></li>
+            <li><img class="panel_menu--image" src="images/logo.png" alt="" /></li>
+            <li><a href="pages/desayunos_economicos.html">Desayunos económicos</a></li>
+            <li><a href="#">Desayunos especiales</a></li>
+            <li><a href="#">Desayunos saludables</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <h1> Desayunos Sorpresa </h1>
+  </div>
+</header>
